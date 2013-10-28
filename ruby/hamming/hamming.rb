8bb30d7c80c5afe3777a505b68453e5efca1faf7
chien <cfeng@us.ibm.com>
2013-10-28 09:46:08 -0500

    hamming on ruby iteration 3

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,18 +1,15 @@
 class Hamming
-
   def self.compute(origin_strand,other_strand)
-    counts = 0
-    long_strand,short_strand = self.order_strand(origin_strand,other_strand)
-    long_strand_arr = long_strand.chars
-    short_strand.chars.each_with_index do |x,i|
-      counts +=  long_strand_arr[i] != x ? 1 : 0 
-    end 
-    counts
+    length = [origin_strand.length,other_strand.length].min
+    (0...length).count { | i | origin_strand[i] != other_strand[i] }
   end
 
-  private
-  def self.order_strand(strand1,strand2)
-    strand1.length > strand2.length ? [ strand1,strand2 ] :  [ strand2,strand1 ]
+  def self.compute2(origin_strand,other_strand)
+    length = [origin_strand.length,other_strand.length].min
+    count = 0
+    (0...length).each do | i |
+      count += origin_strand[i] != other_strand[i] ? 1 : 0
+    end
+    count
   end
-
 end

5f9b50fd931716e9a636daa4821788cf83e12f82
Eric Feng <chienyuan.feng@gmail.com>
2016-11-21 23:13:56 -0600

    fetch new assignments

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,7 +1,23 @@
+
 defmodule Teenager do
+  @moduledoc """
+  Elixir Teenager module 
+  For instance; 
+      def hey(input) do
+      end
+  """
+
+  @doc """
+  Greets the user by name, or by saying "Hello, World!"
+  if no name is given.
+  """
+  @spec hey(String.t) :: String.t
   def hey(input) do
     cond do
-
+      input =~ ~r/^\s*$/ ->  "Fine. Be that way!"
+      input =~ ~r/^[A-Z0-9 %^*@#$(!]+$/ -> "Woah, chill out!"
+      input =~ ~r/\?$/ -> "Sure."
+      true -> "Whatever."
     end
   end
 end

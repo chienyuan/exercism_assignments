caba829438ca9cc67f44c48e3e8e017047318c9e
Eric Feng <chienyuan.feng@gmail.com>
2016-11-25 07:59:35 -0600

    submit 1 space_age elixir

--- elixir/space-age/space_age.exs
+++ elixir/space-age/space_age.exs
@@ -8,6 +8,17 @@ defmodule SpaceAge do
   """
   @spec age_on(planet, pos_integer) :: float
   def age_on(planet, seconds) do
-
+    earth_year = seconds / 365.25 / 24 / 60 / 60
+    cond do
+      planet == :mercury -> earth_year / 0.2408467
+      planet == :venus -> earth_year / 0.61519726
+      planet == :mars -> earth_year / 1.8808158 
+      planet == :jupiter -> earth_year / 11.862615 
+      planet == :saturn -> earth_year / 29.447498
+      planet == :uranus -> earth_year / 84.016846
+      planet == :neptune -> earth_year / 164.79132
+      true -> earth_year
+    end
   end
+  
 end

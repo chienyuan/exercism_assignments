74cd5f517f61b0a59dfb63f65d41d9b010945859
Eric Feng <chienyuan.feng@gmail.com>
2016-11-27 15:46:57 -0600

    submit elixir raindrops iteration 1

--- elixir/raindrops/raindrops.exs
+++ elixir/raindrops/raindrops.exs
@@ -10,6 +10,17 @@ defmodule Raindrops do
   """
   @spec convert(pos_integer) :: String.t
   def convert(number) do
-
+    x = Enum.filter(1..number, &( rem(number,&1) == 0 ) ) 
+    |> Enum.map(&( _convert(&1)))  
+    |> Enum.join
+    cond do
+      x == "" -> to_string(number)
+      true -> x
+    end
   end
+
+  defp _convert(number) when number == 7 , do: "Plong"
+  defp _convert(number) when number == 5 , do: "Plang"
+  defp _convert(number) when number == 3 , do: "Pling"
+  defp _convert(_number)                 , do: ""
 end

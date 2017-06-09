d64837973c64fc2c65fc164065c87aa77703881e
Eric Feng <chienyuan.feng@gmail.com>
2017-06-08 21:29:39 -0500

    solve elixir sum_of_multiples

--- elixir/sum-of-multiples/sum_of_multiples.exs
+++ elixir/sum-of-multiples/sum_of_multiples.exs
@@ -4,6 +4,8 @@ defmodule SumOfMultiples do
   """
   @spec to(non_neg_integer, [non_neg_integer]) :: non_neg_integer
   def to(limit, factors) do
-
+    #pass? = &( 0 < length (factors |> Enum.filter( fn(x) -> rem(&1,x) == 0 end )))
+    pass? = fn(y) ->  0 < length (factors |> Enum.filter( fn(x) -> rem(y,x) == 0 end )) end
+    1..(limit-1) |> Enum.filter(pass?) |> Enum.sum
   end
 end

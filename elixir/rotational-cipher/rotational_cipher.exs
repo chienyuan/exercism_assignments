dc62005f889d09141a3612d7d58173d30b92c210
Eric Feng <chienyuan.feng@gmail.com>
2018-01-20 16:53:43 -0600

    round 1 for elixir rotational_cipher

--- elixir/rotational-cipher/rotational_cipher.exs
+++ elixir/rotational-cipher/rotational_cipher.exs
@@ -8,6 +8,21 @@ defmodule RotationalCipher do
   """
   @spec rotate(text :: String.t(), shift :: integer) :: String.t()
   def rotate(text, shift) do
+    to_string ( text
+    |> String.to_charlist
+    |> Enum.map &(_pick(&1,shift))
+    )
+  end
+
+  defp _pick(c,s) when (c > 96 and c < 133) do
+    97 + rem( (c - 97) + s, 26 )
+  end
+
+  defp _pick(c,s) when (c > 64 and c < 91) do
+    65 + rem( (c - 65) + s, 26 )
+  end
+
+  defp _pick(c,_s) do
+    c
   end
 end
-
